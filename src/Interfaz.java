968d768786e289935ad296d1181411634f02463a
Spotito <jesuseduardo220604@gmail.com>
2024-05-29 23:13:22 -0400

    Interfaz Lista
    Falta que reciba las variables xd

---
+++ src/Interfaz.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Interfaz {
+    private JComboBox MarcaCB;
+    private JComboBox ModeloCB;
+    private JTextField MatriculaTF;
+    private JTextField AñoTF;
+    private JPanel Panel;
+    private JLabel Colorcolor;
+    private JLabel Titulo;
+    private JList Lista;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton elegirFotoButton;
+    private JButton crearButton;
+}
